4088a2d80685dd907cce7de605289f07528e564f
Samarth Kolur <samarthdkolur1@gmail.com>
2026-04-04 00:24:38 +0530

    feat: integrate google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -26,11 +27,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${inter.variable} ${geistMono.variable} h-full`}
-    >
+    <html lang="en" className={`${inter.variable} ${geistMono.variable} h-full`}>
       <body className="grain min-h-full flex flex-col antialiased bg-background text-foreground">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3S2T96R14P"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3S2T96R14P');
+          `}
+        </Script>
         {children}
       </body>
     </html>
